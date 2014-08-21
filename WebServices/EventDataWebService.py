ad78e458f6f9a19bee9aef7e62ac5e4b5f1d6c43
Sam Nicholson <sam.nicholson@ystv.co.uk>
2014-08-22 00:35:16 +0100

    Added a SQLAlchemy engine and enough code to query old Tarantula databases and pull out event details

--- WebServices/EventDataWebService.py
+++ WebServices/EventDataWebService.py
@@ -3,36 +3,40 @@ import string
 
 import cherrypy
 
-from PlaylistEvent import PlaylistEntry, DeviceTypes
+from PlaylistEvent import *
 
 events = [
     PlaylistEntry(10, "test", DeviceTypes.VIDEODEVICE, 1, "Test123", 4, 1, "", None, None, eventid=2),
     PlaylistEntry(10, "test", DeviceTypes.VIDEODEVICE, 1, "Test123", 4, 1, "", None, [PlaylistEntry(10, "test3"),], eventid=3)
 ]
 
+EVENTDATASERVICE_CONF = {
+        '/': {
+            'request.dispatch': cherrypy.dispatch.MethodDispatcher(),
+            'tools.db.on'     : True,
+        }
+    }
+
 class EventDataWebService(object):
     exposed = True
 
     @cherrypy.tools.accept(media='text/plain')
     @cherrypy.tools.json_out()
-    def GET(self):
-        return [e.get_dict() for e in events]
+    def GET(self, eventid):
+        #return [e.get_dict() for e in events]
+        database = cherrypy.request.db
+        
+        dataarray = database.query(PlaylistEntry_SQL).filter(PlaylistEntry_SQL.id == eventid).all()
+        return [e.get_dict() for e in dataarray]
 
-    def POST(self, length=8):
-        some_string = ''.join(random.sample(string.hexdigits, int(length)))
-        cherrypy.session['mystring'] = some_string
-        return some_string
-
-    def PUT(self, another_string):
-        cherrypy.session['mystring'] = another_string
-
-    def DELETE(self):
-        cherrypy.session.pop('mystring', None)
 
 if __name__ == '__main__':
-    conf = {
-        '/': {
-            'request.dispatch': cherrypy.dispatch.MethodDispatcher(),
-        }
-    }
-    cherrypy.quickstart(EventDataWebService(), '/', conf)
+    
+    from SQLEngine import *
+    SAEnginePlugin(cherrypy.engine, 'sqlite:///test3.db').subscribe()
+    cherrypy.tools.db = SATool()
+    #cherrypy.tree.mount(EventDataWebService(), '/api/v1.0/events', EVENTDATASERVICE_CONF)
+    #cherrypy.engine.start()
+    #cherrypy.engine.block()
+    
+    cherrypy.quickstart(EventDataWebService(), '/', EVENTDATASERVICE_CONF)
--- WebServices/PlaylistEvent.py
+++ WebServices/PlaylistEvent.py
@@ -1,8 +1,53 @@
 # Some defined enums
 from misc import enum
 
+from sqlalchemy import Column, DateTime, String, Integer, ForeignKey, func, BigInteger, Text
+from sqlalchemy.orm import *
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm.collections import *
 DeviceTypes = enum(CROSSPOINT="Crosspoint", VIDEODEVICE="Video", CGDEVICE="CG", PROCESSOR="Processor")
 
+Base = declarative_base()
+
+"""(id INTEGER PRIMARY KEY AUTOINCREMENT, type INT, trigger INT64, "
+            "device TEXT, devicetype INT, action, duration INT, parent INT, processed INT, lastupdate INT64, "
+            "callback TEXT, description TEXT"""
+            
+"""(eventid INT, key TEXT, value TEXT, processed INT)"""
+
+class PlaylistEntry_SQL(Base):
+    __tablename__ = 'Default_events'
+    
+    id = Column(Integer, primary_key=True, autoincrement = True)
+    type = Column(Integer)
+    trigger = Column(BigInteger, index=True)
+    device = Column(Text)
+    devicetype = Column(Integer)
+    action = Column(Integer)
+    duration = Column(Integer)
+    parent = Column(Integer, ForeignKey(id))
+    processed = Column(Integer)
+    lastupdate = Column(BigInteger)
+    callback = Column(Text)
+    description = Column(Text)
+    
+    children = relationship("PlaylistEntry_SQL", backref=backref("parent_id", remote_side=id), collection_class=attribute_mapped_collection('id'),)
+    
+    def get_dict(self):
+        """Return a dictionary version of the class for serialization"""
+        return {
+                    'eventid' : self.id,
+                    'time' : self.trigger,
+                    'devicename' : self.device,
+                    'devicetype' : self.devicetype,
+                    'actionid' : self.action,
+                    'description' : self.description,
+                    'duration' : self.duration,
+                    'parentid' : self.parent,
+                    'preprocessor' : self.callback,
+                    
+                    'children' : ([self.children[e].get_dict() for e in self.children]) if self.children != None else []
+                }
 
 class PlaylistEntry(object):
     """Define a playlist entry roughly like it is stored in the database"""
