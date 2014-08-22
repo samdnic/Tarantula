68e1bd7dbe84f877724d00ab61678ab9d748fbaa
Sam Nicholson <sam.nicholson@ystv.co.uk>
2014-08-22 23:29:23 +0100

    Made core mount path more sensible

--- WebServices/TarantulaWebServiceCore.py
+++ WebServices/TarantulaWebServiceCore.py
@@ -4,10 +4,12 @@ from SQLEngine import SAEnginePlugin, SATool
 
 from EventDataWebService import EventDataWebService, EVENTDATASERVICE_CONF
 
+MOUNT_LOCATION = '/api/v1.0'
+
 
 if __name__ == '__main__':
     SAEnginePlugin(cherrypy.engine, 'sqlite:///test3.db').subscribe()
     cherrypy.tools.db = SATool()
-    cherrypy.tree.mount(EventDataWebService(), '/api/v1.0/events', EVENTDATASERVICE_CONF)
+    cherrypy.tree.mount(EventDataWebService(), '{0}/events'.format(MOUNT_LOCATION), EVENTDATASERVICE_CONF)
     cherrypy.engine.start()
     cherrypy.engine.block()
