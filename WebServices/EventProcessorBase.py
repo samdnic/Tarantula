fa3849c6aa66490fbf4d82686f06e57c5a7597ad
Sam Nicholson <sam.nicholson@ystv.co.uk>
2014-08-31 19:52:12 +0100

    Added XML config parsing for all EventProcessors, modded base XML files to match.

--- WebServices/EventProcessorBase.py
+++ WebServices/EventProcessorBase.py
@@ -1,18 +1,36 @@
 from importlib import import_module
+import xml.etree.ElementTree as XMLTree
+import glob
+import os
 
 processorlist = {}
 
 def setup_event_processors(configpath):
     """Load all the available event processors and register them"""
     # Load up and parse some XML files
-    
-    # Work out a list of processors which are on, and load them
-    activeprocessors = ['EventProcessorDemo']
-    
-    for processor in activeprocessors:
-        module = import_module("EventProcessors.{0}".format(processor))
-        proc = getattr(module, processor)()
-        processorlist[processor] = proc
+    confpath = os.path.join('config', configpath, "*.xml")
+    for filename in glob.glob(confpath):
+        configroot = XMLTree.parse(filename).getroot()
+        configdata = configroot.find('PluginLoad')
+        
+        if configdata.find('Enabled').text.lower() == 'true':
+            # Perform some basic validation steps on the config file
+            if configdata.find('Type').text != 'EventProcessor':
+                print "Config file {0} was a bad type {1}".format(filename, configdata.find('Type').text)
+                continue
+                
+            processorname = configdata.find('Instance').text
+            scriptname = configdata.find('ScriptName').text
+            
+            if not processorname or not scriptname:
+                print "Got a blank for Instance or ScriptName in {0}".format(filename)
+                
+            module = import_module("EventProcessors.{0}".format(scriptname))
+            proc = getattr(module, scriptname)(configroot.find('PluginData'))
+            processorlist[processorname] = proc
+        
+        else:
+            print "Got a bad file or disabled plugin for {0}".format(filename)
 
 class EventProcessorBase(object):
     """Base class for event processor objects"""
